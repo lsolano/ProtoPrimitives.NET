44de4618a1f9748da51c3f5a9bc9d38a51b76668
Lorenzo Solano Martínez <lorenzo.sm@gmail.com>
2020-01-06 17:30:43 -0400

    Adding tests for configurable string.

---
+++ src/test/cs/ProtoPrimitives.NET.Tests/Strings/ConfigurableStringFacts/BuilderBuildMessage.cs
@@ -0,0 +1,38 @@
+using System;
+using NUnit.Framework;
+using ProtoPrimitives.NET.Exceptions;
+using ProtoPrimitives.NET.Strings;
+
+namespace ProtoPrimitives.NET.Tests.Strings.ConfigurableStringFacts
+{
+    [TestFixture]
+    internal sealed class BuilderBuildMessage
+    {
+        private static readonly Message ArgNullErrorMessage = new Message("Some caller provided not null message");
+
+        [Test]
+        public void Allows_Build_After_Instantiation([Values(false, true)] bool useSingleParamConstructor, [Values(false, true)] bool useSingleMessage)
+        {
+            Assume.That(useSingleParamConstructor && useSingleMessage, Is.Not.True);
+
+            var builder = useSingleParamConstructor ? new ConfigurableString.Builder(ArgNullErrorMessage) : new ConfigurableString.Builder(ArgNullErrorMessage, useSingleMessage);
+            const string rawValue = "I'm a valid string";
+
+            ConfigurableString str = builder.Build(rawValue);
+
+            Assert.That(str.Value, Is.SameAs(rawValue));
+        }
+
+        [Test]
+        public void Throws_If_Called_Twice([Values(false, true)] bool useSingleParamConstructor, [Values(false, true)] bool useSingleMessage)
+        {
+            Assume.That(useSingleParamConstructor && useSingleMessage, Is.Not.True);
+
+            var builder = useSingleParamConstructor ? new ConfigurableString.Builder(ArgNullErrorMessage) : new ConfigurableString.Builder(ArgNullErrorMessage, useSingleMessage);
+
+            _ = builder.Build("I'm a valid string");
+
+            Assert.That(() => builder.Build("Some other value."), Throws.InstanceOf<InvalidOperationException>().With.Message.EqualTo("Already built."));
+        }
+    }
+}
