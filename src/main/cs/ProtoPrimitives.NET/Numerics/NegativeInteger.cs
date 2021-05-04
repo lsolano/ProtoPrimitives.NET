9dbbcc55a0294ecf90ce750a20bf2b5719631283
Lorenzo Solano Martínez <lorenzo.sm@gmail.com>
2021-05-04 15:53:17 -0400

    Fixes: Restore IEquatable and IComparable contracts to PositiveInteger and NegativeInteger.

--- src/main/cs/ProtoPrimitives.NET/Numerics/NegativeInteger.cs
+++ src/main/cs/ProtoPrimitives.NET/Numerics/NegativeInteger.cs
@@ -8,7 +8,7 @@ namespace Triplex.ProtoDomainPrimitives.Numerics
     /// <summary>
     /// Valid negative integer, meaning <code>&lt; 0</code> (less than zero).
     /// </summary>
-    public sealed class NegativeInteger : AbstractDomainPrimitive<int>
+    public sealed class NegativeInteger : AbstractDomainPrimitive<int>, IEquatable<NegativeInteger>, IComparable<NegativeInteger>
     {
         /// <summary>
         /// Error message used when not provided.
@@ -40,6 +40,78 @@ namespace Triplex.ProtoDomainPrimitives.Numerics
         {
         }
 
-        private static int Validate(in int rawValue, in Message errorMessage) => Arguments.LessThan(rawValue, 0, nameof(rawValue), errorMessage.Value);
+        private static int Validate(in int rawValue, in Message errorMessage)
+            => Arguments.LessThan(rawValue, 0, nameof(rawValue), errorMessage.Value);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.CompareTo(IDomainPrimitive{TRawType}?)" />
+        public int CompareTo(NegativeInteger? other) => base.CompareTo(other);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.Equals(object?)" />
+        public override bool Equals(object? obj) => Equals(obj as NegativeInteger);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.Equals(IDomainPrimitive{TRawType}?)" />
+        public bool Equals(NegativeInteger? other) => base.Equals(other);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.GetHashCode()" />
+        public override int GetHashCode() => base.GetHashCode();
+
+
+        #region Relational Operators
+
+        /// <summary>
+        /// Indicates if two instances are not equal.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(in NegativeInteger left, in NegativeInteger right)
+            => RelationalOperatorsOverloadHelper.NotEquals<NegativeInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if two instances are equals.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(in NegativeInteger left, in NegativeInteger right)
+            => RelationalOperatorsOverloadHelper.Equals<NegativeInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is less than <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator <(in NegativeInteger left, in NegativeInteger right)
+            => RelationalOperatorsOverloadHelper.LessThan<NegativeInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is less than or equals to <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator <=(in NegativeInteger left, in NegativeInteger right)
+            => RelationalOperatorsOverloadHelper.LessThanOrEqualsTo<NegativeInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is greater than <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator >(in NegativeInteger left, in NegativeInteger right)
+            => RelationalOperatorsOverloadHelper.GreaterThan<NegativeInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is greater than or equals to <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator >=(in NegativeInteger left, in NegativeInteger right)
+            => RelationalOperatorsOverloadHelper.GreaterThanOrEqualsTo<NegativeInteger>(left, right);
+        
+        #endregion //Relational Operators
     }
 }
--- src/main/cs/ProtoPrimitives.NET/Numerics/PositiveInteger.cs
+++ src/main/cs/ProtoPrimitives.NET/Numerics/PositiveInteger.cs
@@ -8,7 +8,7 @@ namespace Triplex.ProtoDomainPrimitives.Numerics
     /// <summary>
     /// Valid positive integer, meaning <code>&gt;= 1</code> (greater than or equals to one).
     /// </summary>
-    public sealed class PositiveInteger : AbstractDomainPrimitive<int>
+    public sealed class PositiveInteger : AbstractDomainPrimitive<int>, IEquatable<PositiveInteger>, IComparable<PositiveInteger>
     {
         /// <summary>
         /// Error message used when not provided.
@@ -42,5 +42,76 @@ namespace Triplex.ProtoDomainPrimitives.Numerics
 
         private static int Validate(in int rawValue, in Message errorMessage)
             => Arguments.GreaterThan(rawValue, 0, nameof(rawValue), errorMessage.Value);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.CompareTo(IDomainPrimitive{TRawType}?)" />
+        public int CompareTo(PositiveInteger? other) => base.CompareTo(other);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.Equals(object?)" />
+        public override bool Equals(object? obj) => Equals(obj as PositiveInteger);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.Equals(IDomainPrimitive{TRawType}?)" />
+        public bool Equals(PositiveInteger? other) => base.Equals(other);
+
+        /// <inheritdoc cref="AbstractDomainPrimitive{TRawType}.GetHashCode()" />
+        public override int GetHashCode() => base.GetHashCode();
+
+
+        #region Relational Operators
+
+        /// <summary>
+        /// Indicates if two instances are not equal.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(in PositiveInteger left, in PositiveInteger right)
+            => RelationalOperatorsOverloadHelper.NotEquals<PositiveInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if two instances are equals.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(in PositiveInteger left, in PositiveInteger right)
+            => RelationalOperatorsOverloadHelper.Equals<PositiveInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is less than <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator <(in PositiveInteger left, in PositiveInteger right)
+            => RelationalOperatorsOverloadHelper.LessThan<PositiveInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is less than or equals to <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator <=(in PositiveInteger left, in PositiveInteger right)
+            => RelationalOperatorsOverloadHelper.LessThanOrEqualsTo<PositiveInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is greater than <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator >(in PositiveInteger left, in PositiveInteger right)
+            => RelationalOperatorsOverloadHelper.GreaterThan<PositiveInteger>(left, right);
+
+        /// <summary>
+        /// Indicates if <paramref name="left"/> is greater than or equals to <paramref name="right"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator >=(in PositiveInteger left, in PositiveInteger right)
+            => RelationalOperatorsOverloadHelper.GreaterThanOrEqualsTo<PositiveInteger>(left, right);
+        
+        #endregion //Relational Operators
     }
 }
